53184c8813013269c1c88333d2474beabc61f280
JngMkk <kjm0817ss@gmail.com>
2023-04-30 19:41:23 +0900

    feat: RabbitMQ's Alternate-Exchange test

---
+++ alternate_exchange/alternate_exchange_pika.py
@@ -0,0 +1,46 @@
+from pika import (
+    PlainCredentials,
+    ConnectionParameters,
+    BlockingConnection,
+    BasicProperties,
+)
+from pika.exchange_type import ExchangeType
+
+import sys
+import os
+
+sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
+
+from config.config_pika import USERNAME, PASSWORD, HOST, PORT  # noqa:E402
+
+
+CREDENTIALS = PlainCredentials(username=USERNAME, password=PASSWORD)
+PARAMS = ConnectionParameters(host=HOST, port=PORT, credentials=CREDENTIALS)
+
+
+with BlockingConnection(parameters=PARAMS) as conn:
+    channel = conn.channel()
+
+    alter_exchange = channel.exchange_declare(
+        exchange="alter_exchange",
+        exchange_type=ExchangeType.fanout,
+    )
+
+    exchange = channel.exchange_declare(
+        exchange="main_exchange",
+        exchange_type=ExchangeType.topic,
+        arguments={"alternate-exchange": "alter_exchange"},
+    )
+
+    queue = channel.queue_declare(queue="unroutable-messages")
+    if channel.queue_bind(
+        queue="unroutable-messages", exchange="alter_exchange", routing_key="#"
+    ):
+        print("Queue bound to alternate-exchange")
+
+    channel.basic_publish(
+        exchange="main_exchange",
+        routing_key="",
+        body="test",
+        properties=BasicProperties(content_type="text/plain"),
+    )
---
+++ alternate_exchange/alternate_exchange_rabbitpy.py
@@ -0,0 +1,49 @@
+from rabbitpy import Connection, Exchange, Queue, Message
+
+import sys
+import os
+
+sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
+
+from config.config_rabbitpy import URL  # noqa:E402
+
+
+with Connection(URL) as conn:
+    with conn.channel() as channel:
+        # * 대체 Exchange 객체 생성
+        alter_exchange = Exchange(
+            channel=channel, name="alter_exchange", exchange_type="fanout"
+        )
+        # * 대체 Exchange 선언
+        alter_exchange.declare()
+
+        exchange = Exchange(
+            channel=channel,
+            name="main_exchange",
+            exchange_type="topic",
+            arguments={"alternate-exchange": alter_exchange.name},
+        )
+
+        exchange.declare()
+
+        # * 라우팅될 수 없는 메시지는 아래 Queue에 저장됨.
+        queue = Queue(channel=channel, name="unroutable-messages")
+        queue.declare()
+
+        if queue.bind(alter_exchange, "#"):
+            print("Queue bound to alternate-exchange")
+
+        msg = Message(
+            channel=channel,
+            body_value="test",
+            properties={"content_type": "text/plain"},
+        )
+        msg.publish(exchange=exchange)
+        # * Queue bound to alternate-exchange
+
+
+"""
+Fanout: 자신이 알고 있는 모든 Queue에 메시지를 전달함.
+
+Topic: 라우팅 키를 기반으로 선택적으로 메시지를 라우팅할 수 있음.
+"""
