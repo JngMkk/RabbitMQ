f536482cce7c1a73f8ff0285b2b65d0933dbc939
JngMkk <kjm0817ss@gmail.com>
2023-05-02 00:13:46 +0900

    feat: RabbitMQ's Consume/Get test

--- Consume_Get/Consume_No_Ack_rabbitpy.py
+++ Consume_Get/Consume_No_Ack_rabbitpy.py
@@ -11,5 +11,8 @@ from config.config_rabbitpy import URL  # noqa:E402
 with Connection(URL) as conn:
     with conn.channel() as channel:
         queue = Queue(channel=channel, name="test-queue")
+
+        # * no_ack=True로 Basic.Consume RPC 요청 전송
+        # * 소비자에게 메시지를 전달하는 가장 빠른 방법
         for msg in queue.consume_messages(no_ack=True):
             msg.pprint()
