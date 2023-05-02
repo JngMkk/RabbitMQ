10c5fe46b725478e204c8524f81508d4167fc817
JngMkk <kjm0817ss@gmail.com>
2023-05-03 00:12:53 +0900

    feat: RabbitMQ's queue controlling ways

---
+++ Controlling_Queue/auto-expiring_rabbitpy.py
@@ -0,0 +1,31 @@
+from rabbitpy import Connection, Queue
+import time
+from rabbitpy.exceptions import AMQPNotFound
+
+import sys
+import os
+
+sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
+
+from config.config_rabbitpy import URL  # noqa:E402
+
+
+with Connection(URL) as conn:
+    with conn.channel() as channel:
+        queue = Queue(
+            channel=channel,
+            name="expiring-test",
+            arguments={"x-expires": 1000},  # * 1초
+        )
+        queue.declare()
+
+        # * 패시브 Queue 선언을 사용해 Queue의 메시지 및 소비자 수 얻기
+        messages, consumers = queue.declare(passive=True)
+        print(messages, consumers)
+        time.sleep(2)
+
+        try:
+            messages, consumers = queue.declare(passive=True)
+            print(messages, consumers)
+        except AMQPNotFound:
+            print("The Queue no longer exists")
---
+++ Controlling_Queue/max-length_rabbitpy.py
@@ -0,0 +1,19 @@
+from rabbitpy import Connection, Queue
+
+import sys
+import os
+
+sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
+
+from config.config_rabbitpy import URL  # noqa:E402
+
+
+with Connection(URL) as conn:
+    with conn.channel() as channel:
+        queue = Queue(
+            channel=channel,
+            name="max-length-test",
+            arguments={"x-max-length": 1000},
+        )
+
+        queue.declare()
