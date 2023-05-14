88af5527934e6a5d7d7c9b5f4327800bd9a81cac
JngMkk <kjm0817ss@gmail.com>
2023-05-14 17:15:10 +0900

    feat: RabbitMQ's publishes

---
+++ publishes/transaction/transaction_rabbitpy.py
@@ -0,0 +1,42 @@
+from rabbitpy import Connection, Tx, Message
+from rabbitpy.exceptions import NoActiveTransactionError
+
+import sys
+import os
+
+sys.path.append(
+    os.path.dirname(
+        os.path.abspath(
+            os.path.dirname(os.path.abspath(os.path.dirname(__file__)))
+        )
+    )
+)
+
+from config.config_rabbitpy import URL, EXCHANGE, ROUTING_KEY
+
+
+with Connection(URL) as conn:
+    with conn.channel() as channel:
+        # * 트랜잭션 객체 생성
+        tx = Tx(channel=channel)
+
+        # * 트랜잭션 시작
+        tx.select()
+
+        msg = Message(
+            channel=channel,
+            body_value="transaction test message",
+            properties={
+                "content_type": "text/plain",
+                "delivery_mode": 2,
+                "message_type": "transaction",
+            },
+        )
+
+        msg.publish(exchange=EXCHANGE, routing_key=ROUTING_KEY)
+
+        try:
+            if tx.commit():
+                print("Transaction committed")
+        except NoActiveTransactionError:
+            print("Tried to commit without active transaction")
